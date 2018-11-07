c5ed2f7a35fd0750f130c8d1cab756b641a5cd17
AzureAD\AdemHoxha <adem.hoxha@hotmail.it>
2018-11-07 17:40:31 +0100

    type reference fix

--- hotel-room-gateway-aggregate/src/main/java/it/watchmefly/hotelroomapp/room/hotelroomagg/controller/HotelRoomController.java
+++ hotel-room-gateway-aggregate/src/main/java/it/watchmefly/hotelroomapp/room/hotelroomagg/controller/HotelRoomController.java
@@ -1,12 +1,16 @@
 package it.watchmefly.hotelroomapp.room.hotelroomagg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
 
 import reactor.core.publisher.Mono;
 
@@ -18,8 +22,9 @@ public class HotelRoomController {
     private WebClient.Builder webClientBuilder;
 	
 	@GetMapping
-	private Mono<ResponseEntity> getHotel(@RequestParam("hotelId") String hoteld) {
-		/*Mono<ResponseEntity<List<Object>>>*/ Mono<ResponseEntity> hotelSearch =  webClientBuilder.build().get().uri("http://hotel/hotel/{hoteld}", hoteld).retrieve().bodyToMono(ResponseEntity.class);		
+	private Mono<ResponseEntity<List<Object>>> getAllRooms() {
+		ParameterizedTypeReference<ResponseEntity<List<Object>>> typeRef = new ParameterizedTypeReference<ResponseEntity<List<Object>>>() {};
+		Mono<ResponseEntity<List<Object>>> hotelSearch =  webClientBuilder.build().get().uri("http://open-room/room/all").retrieve().bodyToMono(typeRef);		
 		return hotelSearch;
 	}
 	
