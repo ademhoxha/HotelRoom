3c2d8724842e44f8c1a3f90d6be9c720710465df
AzureAD\AdemHoxha <adem.hoxha@hotmail.it>
2018-11-14 21:39:47 +0100

    cloud config server fix

--- hotel/src/main/java/it/watchmefly/hotelroomapp/room/hotel/controller/ServerConfigController.java
+++ hotel/src/main/java/it/watchmefly/hotelroomapp/room/hotel/controller/ServerConfigController.java
@@ -1,24 +1,21 @@
 package it.watchmefly.hotelroomapp.room.hotel.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.watchmefly.hotelroomapp.room.hotel.config.ServerBean;
-import it.watchmefly.hotelroomapp.room.hotel.config.ServerConfig;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("config")
 public class ServerConfigController {
 	
-	ApplicationContext ctx = new AnnotationConfigApplicationContext(ServerConfig.class);
+	@Value("${config.version: Not Versioned}")
+	private String version;
 
 	@GetMapping("/version")
 	private Mono<String> getVersion(){ // mono is not needed at all
-		return Mono.just( ((ServerBean) ctx.getBean(ServerBean.class)).getServerVersion());
+		return Mono.just(this.version);
 	}
-	
 }
